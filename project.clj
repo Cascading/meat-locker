3614c3b32de470d8376b0dc65638f3ed69243ca3
Sam Ritchie <sritchie09@gmail.com>
2012-02-14 10:23:06 -0800

    Added in hadoop as a dev dependency.

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
   :dev-dependencies [[org.clojure/clojure "1.3.0"]
                      [org.apache.thrift/libthrift "0.6.1"]
                      [com.google.protobuf/protobuf-java "2.4.0a"]
+                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [cascading/cascading-hadoop "2.0.0-wip-184"
                       :exclusions [org.codehaus.janino/janino
                                    org.apache.hadoop/hadoop-core]]])
