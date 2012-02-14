7ce78e3c65912ceef9e79017001a864e3ca0a65d
Sam Ritchie <sritchie09@gmail.com>
2012-02-14 10:12:03 -0800

    Added cascading as a dev dependency.

--- project.clj
+++ project.clj
@@ -6,4 +6,7 @@
                  [com.googlecode/kryo "1.04"]]
   :dev-dependencies [[org.clojure/clojure "1.3.0"]
                      [org.apache.thrift/libthrift "0.6.1"]
-                     [com.google.protobuf/protobuf-java "2.4.0a"]])
+                     [com.google.protobuf/protobuf-java "2.4.0a"]
+                     [cascading/cascading-hadoop "2.0.0-wip-184"
+                      :exclusions [org.codehaus.janino/janino
+                                   org.apache.hadoop/hadoop-core]]])
