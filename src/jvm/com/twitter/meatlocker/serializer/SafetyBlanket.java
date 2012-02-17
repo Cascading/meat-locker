91d438d3bd4a216da1de63a8128a7bc277063636
Sam Ritchie <sritchie09@gmail.com>
2012-02-16 18:08:53 -0800

    limited the scope of Timid.

--- src/jvm/com/twitter/meatlocker/serializer/SafetyBlanket.java
+++ src/jvm/com/twitter/meatlocker/serializer/SafetyBlanket.java
@@ -1,8 +1,6 @@
 package com.twitter.meatlocker.serializer;
 
-import org.apache.hadoop.conf.Configurable;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.io.serializer.Deserializer;
 import org.apache.hadoop.io.serializer.Serialization;
 import org.apache.hadoop.io.serializer.SerializationFactory;
@@ -10,18 +8,21 @@ import org.apache.hadoop.io.serializer.Serializer;
 import org.apache.hadoop.mapred.JobConf;
 
 /** User: sritchie Date: 2/16/12 Time: 2:24 PM */
-public class SafetyBlanket<T> extends Configured implements Serialization<T> {
+public class SafetyBlanket<T> implements Serialization<T> {
     public static final String IO_SERIALIZATIONS = "io.serializations";
-    Serialization<T> wrapped;
     SerializationFactory factory;
+    Configuration conf;
 
-    public SafetyBlanket(Serialization<T> wrapped) {
-        this.wrapped = wrapped;
+    public SafetyBlanket(Configuration conf, Class<? extends Serialization> wrapped) {
+        Configuration config = (conf != null) ? conf : new JobConf();
+        this.conf = processConf(config, wrapped);
     }
 
-    public void processConf(Configuration conf) {
+    public static Configuration processConf(Configuration oldConf, Class klass) {
+        Configuration conf = new Configuration(oldConf);
         // This key won't ever be null, since we know that THIS serialization is registered.
-        String[] serializations = conf.get(IO_SERIALIZATIONS).split(",");
+        String serString = conf.get(IO_SERIALIZATIONS);
+        String[] serializations = serString.split(",");
 
         StringBuilder builder = new StringBuilder();
         boolean encountered = false;
@@ -35,40 +36,28 @@ public class SafetyBlanket<T> extends Configured implements Serialization<T> {
                 builder.append(serialization);
             }
 
-            if (serialization.equals(this.getClass().getName()))
+            if (serialization.equals(klass.getName()))
                 encountered = true;
         }
 
-        conf.set(IO_SERIALIZATIONS, builder.toString());
-    }
+        String newVal = builder.toString();
 
-    @Override public void setConf( Configuration conf ) {
-        processConf(conf);
-        if (wrapped instanceof Configurable)
-            ((Configurable) wrapped).setConf(conf);
-        else
-            super.setConf(conf);
-    }
-
-    @Override public Configuration getConf() {
-        if( super.getConf() == null )
-            setConf( new JobConf() );
-        return (wrapped instanceof Configurable) ? ((Configurable) wrapped).getConf() : super.getConf();
+        // If the serialization simply wasn't present, set the old value.
+        conf.set(IO_SERIALIZATIONS, newVal.equals("") ? serString : newVal);
+        return conf;
     }
 
     public boolean accept(Class<?> aClass) {
         if (factory == null)
-            factory = new SerializationFactory(getConf());
-        return (factory.getSerialization(aClass) != null || wrapped.accept(aClass));
+            factory = new SerializationFactory(conf);
+        return factory.getSerialization(aClass) != null;
     }
 
     public Serializer<T> getSerializer(Class<T> aClass) {
-        Serializer<T> factorySerializer = factory.getSerializer(aClass);
-        return (factorySerializer != null) ? factorySerializer : wrapped.getSerializer(aClass);
+        return factory.getSerializer(aClass);
     }
 
     public Deserializer<T> getDeserializer(Class<T> aClass) {
-        Deserializer<T> factoryDeserializer = factory.getDeserializer(aClass);
-        return (factoryDeserializer != null) ? factoryDeserializer : wrapped.getDeserializer(aClass);
+        return factory.getDeserializer(aClass);
     }
 }
--- src/jvm/com/twitter/meatlocker/serializer/TimidSerialization.java
+++ src/jvm/com/twitter/meatlocker/serializer/TimidSerialization.java
@@ -1,6 +1,5 @@
 package com.twitter.meatlocker.serializer;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.io.serializer.Deserializer;
 import org.apache.hadoop.io.serializer.Serialization;
@@ -8,31 +7,19 @@ import org.apache.hadoop.io.serializer.Serializer;
 
 /** User: sritchie Date: 2/16/12 Time: 4:55 PM */
 public abstract class TimidSerialization<T> extends Configured implements Serialization<T> {
-
     SafetyBlanket<T> blanket;
 
-    public abstract Serialization<T> getWrappedSerialization();
-
-    public final boolean accept(Class<?> aClass) {
-        if(blanket == null) {
-            blanket = new SafetyBlanket<T>(getWrappedSerialization());
-        }
+    public boolean accept(Class<?> aClass) {
+        if(blanket == null)
+            blanket = new SafetyBlanket<T>(this.getConf(), this.getClass());
         return blanket.accept(aClass);
     }
 
-    @Override public final void setConf( Configuration conf ) {
-        blanket.setConf(conf);
-    }
-
-    @Override public final Configuration getConf() {
-        return blanket.getConf();
-    }
-
-    public final Serializer<T> getSerializer(Class<T> tClass) {
+    public Serializer<T> getSerializer(Class<T> tClass) {
         return blanket.getSerializer(tClass);
     }
 
-    public final Deserializer<T> getDeserializer(Class<T> tClass) {
+    public Deserializer<T> getDeserializer(Class<T> tClass) {
         return blanket.getDeserializer(tClass);
     }
 }
