dab8a5cb10870e2cbd2be971debc07048bf6a436
Sam Ritchie <sritchie09@gmail.com>
2012-02-19 13:26:08 -0800

    Twitter prefix.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject meat-locker "0.1.4"
+(defproject com.twitter/meat-locker "0.1.4"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :description "Grab bag of utilities for Cascading DSLs."
