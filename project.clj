70c4984036382ca8b66e16d7968de63415f1fe9a
Sam Ritchie <sritchie09@gmail.com>
2012-04-11 23:37:29 -0700

    Snapshot version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.twitter/meat-locker "0.2.0"
+(defproject com.twitter/meat-locker "0.2.1-SNAPSHOT"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :description "Grab bag of utilities for Cascading DSLs."
