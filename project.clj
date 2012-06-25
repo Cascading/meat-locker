d1be67c5e21f687144559628c950adf92061ec7a
Sam Ritchie <sritchie09@gmail.com>
2012-06-25 10:50:17 -0700

    Snapshot bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.twitter/meat-locker "0.3.0"
+(defproject com.twitter/meat-locker "0.3.1-SNAPSHOT"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :description "Serializers for Kryo."
