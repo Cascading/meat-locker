260a2836c23998659f3ccdccaa4ea3e186ea31b9
Sam Ritchie <sritchie09@gmail.com>
2012-08-07 11:55:44 -0700

    kryo 2.17.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :description "Serializers for Kryo."
-  :dependencies [[com.esotericsoftware.kryo/kryo "2.16"]]
+  :dependencies [[com.esotericsoftware.kryo/kryo "2.17"]]
   :dev-dependencies [[midje "1.3.1" :exclusions [org.clojure/clojure]]
                      [org.clojure/clojure "1.2.1"]
                      [org.apache.thrift/libthrift "0.6.1"]
